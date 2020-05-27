bfb9482b1b6a2fd290507a1c50aee6a89eb5758f
mya9 <imohduni@gmail.com>
2020-05-27 07:44:27 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -114,29 +114,8 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Contact</h2>
-          <form method="post" action="https://www.drmohd.me/">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <h2 className="major">Email: imohduni@gmail.com</h2>
+         
           <ul className="icons">
             <li>
               <a
