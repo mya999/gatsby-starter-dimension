5d0e8322d2e59a3bc1304f86fd0db2186ba0b8dc
mya9 <imohduni@gmail.com>
2020-05-27 07:46:22 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from 'react'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
-      <span className="icon fa-diamond"></span>
-    </div>
+    
     <div className="content">
       <div className="inner">
         <h1>Dr.Mohammed Y. Mardi</h1>
